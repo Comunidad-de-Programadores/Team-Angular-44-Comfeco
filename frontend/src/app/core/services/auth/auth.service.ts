083315dcd2e8f374cd1b90bd20fb5c602de6cbe5
IramML <irammartinezlamas@gmail.com>
2021-02-13 11:46:01 -0600

    Add auth service functionality

--- frontend/src/app/core/services/auth/auth.service.ts
+++ frontend/src/app/core/services/auth/auth.service.ts
@@ -1,10 +1,39 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from '../../models/user/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  usersRef: AngularFireObject<any>;
 
-  signIn() {}
+  constructor(private firebaseAuth: AngularFireAuth, private firebaseDatabase: AngularFireDatabase) {}
+
+  signIn(email: string, password: string) {
+    return this.firebaseAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  async signUp(user: Partial<User>, password: string) {
+    const data = await this.firebaseAuth.createUserWithEmailAndPassword(user.email, password);
+    return await this.saveUserData(user, data.user.uid);
+  }
+
+  private saveUserData(user: Partial<User>, userId: string) {
+    this.usersRef = this.firebaseDatabase.object(`users/${userId}`);
+    return this.usersRef.set(user);
+  }
+
+  recoverPassword(email: string) {
+    return this.firebaseAuth.sendPasswordResetEmail(email);
+  }
+
+  logout() {
+    return this.firebaseAuth.signOut();
+  }
+
+  hasUser() {
+    return this.firebaseAuth.authState;
+  }
 }
