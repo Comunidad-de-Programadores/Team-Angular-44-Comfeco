72fed3cd31ab591ec08d00be54e619421617e81d
IramML <irammartinezlamas@gmail.com>
2021-02-13 12:47:37 -0600

    Add privacy policy, terms and conditions policy

---
+++ frontend/src/app/policy/policy.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PolicyRoutingModule } from './policy-routing.module';
+import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { PoliciesComponent } from './policies/policies.component';
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../material/material.module';
+
+@NgModule({
+  declarations: [TermsConditionsComponent, PrivacyComponent, PoliciesComponent],
+  imports: [CommonModule, PolicyRoutingModule, CoreModule, SharedModule, MaterialModule],
+})
+export class PolicyModule {}
