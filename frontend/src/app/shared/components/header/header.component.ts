c8b57570f6082eaa5c969e66a953ecc7f0ff3c3d
heroesh <fabio.quintero@gmail.com>
2021-02-14 21:51:44 -0500

    Add Header

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router) {
+
+    console.log(router.url);
+  }
 
   ngOnInit(): void {
+  }
+
+  isRecoverPasswordPath() {
+    return this.router.url === "/auth/recover-password";
   }
 
 }
