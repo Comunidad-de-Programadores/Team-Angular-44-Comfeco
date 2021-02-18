cad36424eb5303263e52483bfb8f70b7c2f20daf
IramML <irammartinezlamas@gmail.com>
2021-02-17 21:05:28 -0600

    Add sign in integration

--- frontend/src/app/auth/sign-in/sign-in.component.ts
+++ frontend/src/app/auth/sign-in/sign-in.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
   form: FormGroup;
   hidePassword: boolean;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private snackbar: MatSnackBar,
+    private router: Router
+  ) {
     this.hidePassword = true;
     this.initForm();
   }
 
   ngOnInit(): void {}
 
-  initForm(): void{
+  initForm(): void {
     this.form = this.formBuilder.group({
-      email: [null, [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$')]],
-      password: [null, [Validators.required, Validators.minLength(6)]]
-    })
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+    });
   }
   get errorEmailRequired(): boolean {
     return this.form.get('email').errors?.required && this.form.get('email').touched;
@@ -27,14 +35,31 @@ export class SignInComponent implements OnInit {
   get errorEmailFormat(): boolean {
     return this.form.get('email').errors?.pattern && this.form.get('email').touched;
   }
-  get errorPasswordRequired(): boolean{
+  get errorPasswordRequired(): boolean {
     return this.form.get('password').errors?.required && this.form.get('password').touched;
   }
-  get errorPasswordLength(): boolean{
+  get errorPasswordLength(): boolean {
     return this.form.get('password').errors?.minlength && this.form.get('password').touched;
   }
 
-  singIn():void {
-    console.log(this.form.valid)
+  async singIn(): Promise<void> {
+    if (this.form.valid) {
+      let { email, password } = this.form.value;
+      try {
+        await this.authService.signIn(email, password);
+        this.router.navigate(['/']);
+      } catch (err) {
+        if (err.code == 'auth/user-not-found') {
+          this.snackbar
+            .open(err.message, 'Iniciar Sesión', { duration: 4000 })
+            .onAction()
+            .subscribe(() => {
+              this.router.navigate(['/auth/sign-up']);
+            });
+        } else {
+          this.snackbar.open(err.message, 'Cerrar', { duration: 4000 });
+        }
+      }
+    }
   }
 }
