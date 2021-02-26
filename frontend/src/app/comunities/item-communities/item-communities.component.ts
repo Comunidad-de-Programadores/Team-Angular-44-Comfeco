d424cd734605b9313a6425a7c781ed9cde42d7b5
Deymer Hernández <deymerh@hotmail.com>
2021-02-26 11:15:13 -0500

    feature/communities

---
+++ frontend/src/app/comunities/item-communities/item-communities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-communities',
+  templateUrl: './item-communities.component.html',
+  styleUrls: ['./item-communities.component.scss']
+})
+export class ItemCommunitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
